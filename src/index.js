0e214449a5fbb9f04053995f885e5c04de6ad189
beckyphan <beckyphan@live.com>
2020-05-21 21:01:52 -0700

    updated index.html with login nav, updated js with querySelectors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-const endPoint = "http://localhost:3000/api/v1/foods";
+const users = "http://localhost:3000/users"
+const foods = "http://localhost:3000/api/v1/foods"
+const nutrients = "http://localhost:3000/api/v1/nutrients"
+const dailylogs = "http://localhost:3000/api/v1/dailylogs"
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  getFoods()
-});
+  const nav = document.querySelector("nav")
+  const loginDiv = document.querySelector("#login")
+  const loginSubmit = document.querySelector("#loginSubmit")
 
-function getFoods() {
-  fetch(endPoint)
-  .then(resp => resp.json())
-  .then(json => console.log(json));
-}
+  UserLogin()
+
+  function loginSubmit.addEventListenern("submit", (e) => {
+    e.preventDefault()
+    let bodyData = {
+      username,
+      password
+    }
+    fetch(users, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(bodyData)
+    })
+    .then(resp => resp.json())
+    .then(json => {
+
+      // nav.innerHTML = renderLogin()
+
+    })
+  }
+
+
+});
