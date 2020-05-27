aa8d7542ffd262ff4e51d2c4daafa82a583f3ff1
beckyphan <beckyphan@live.com>
2020-05-27 00:12:12 -0700

    added notes on next steps on what needs to be viewed

--- src/index.js
+++ src/index.js
@@ -37,7 +37,8 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log("test")
       let loggedInUser = new User(user.data, user.data.attributes)
       nav.innerHTML = loggedInUser.renderWelcome()
-
+      driReading.innerHTML =
+      `<h4>my daily values:</h4>`
       renderDri(loggedInUser)
 
       // if logged in, then render user's profile daily logs
@@ -58,12 +59,19 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(profile)
       for (let [key, value] of Object.entries(profile)) {
         driReading.innerHTML +=
-        `<span>
+        `<span class="dailyvalue-${key}">
             ${key}: ${value}
-          </span>
-        `
+          </span><br>`
       }
     })
   }
+
+  // function to show user's daily logs - show most recent 3 days + addtoday if not already existing
+  //  for all forms, user is able to add a food (by typing in) and it searches for the food
+  // need to enter food & serving # => calculates the nutrient profile of the food based on the template
+  //  needs to create a button that calulates how much the user has consumed & how much more needed
+  // function renderDailyLogForm(user) {
+  //
+  // }
 
 });
